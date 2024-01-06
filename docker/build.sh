f6a5150b0e6dd0ca7deda3cfb1449db384476a2c
Ubuntu <lowinli@stable-diffustion.ozaokuv3isuujm213a14raejgh.milx.internal.cloudapp.net>
2024-01-06 05:38:44 +0000

    fix download and quantize model in docker image

--- docker/build.sh
+++ docker/build.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
+set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILDROOT=$DIR/..
  
 cd $BUILDROOT
  
 CONTAINER="lowinli98/stable-diffusion-quantized"
-VERSION="0.0.4"
+VERSION="0.0.5"
  
-IMAGE_NAME="${CONTAINER}:${VERSION}"
+BASE_IMAGE_NAME="${CONTAINER}:${VERSION}"
 
-cmd="docker build -t $IMAGE_NAME -f $DIR/dockerfile $BUILDROOT"
+cmd="docker build -t $BASE_IMAGE_NAME -f $DIR/dockerfile $BUILDROOT"
 echo $cmd
 eval $cmd
 
-# docker push $IMAGE_NAME
+OPEN_MIDJOUNEY_IMAGE_NAME="${CONTAINER}:open-midjourney-${VERSION}"
+
+
+MODEL_IDS=("prompthero/openjourney" "CompVis/stable-diffusion-v1-4" "runwayml/stable-diffusion-v1-5" "stabilityai/stable-diffusion-2" "stabilityai/stable-diffusion-2-1")
+for MODEL_ID in "${MODEL_IDS[@]}"
+do
+    TAG=${VERSION}-"${MODEL_ID//\//-}"
+    IMAGE_NAME_WITH_MODEL="${CONTAINER}:${TAG}"
+    echo "Building... ${IMAGE_NAME_WITH_MODEL}"
+    docker build -t $IMAGE_NAME_WITH_MODEL \
+        -f $DIR/dockerfile-with-model-quantized \
+        --build-arg MODEL_ID=${MODEL_ID} \
+        --build-arg BASE_IMAGE=${BASE_IMAGE_NAME} \
+        $BUILDROOT
+    echo "Finished $name"
+done
